76066a3222124fcaefea63baca79497b3e9ff131
˜nthome˜ <nataliathome@gmail.com>
2025-02-24 20:49:18 +0000

    added event to button and handle event function

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addItemToCart } from './CartSlice';
 
 const ProductList = () => {
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
+
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+
+
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...disabledProducts, product.id]); // Mark the product as disabled
+  };
 
   return (
     <div className="product-list">
@@ -16,11 +28,15 @@ const ProductList = () => {
      {products.map(product => (
       <li key={product.id} className="product-list-item">
       <span>{product.name} - ${product.price}</span>
-      <button>
+        <button
+        className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+        onClick={() => handleAddToCart(product)}
+        disabled={disabledProducts.includes(product.id)} // Disable button if product is in disabledProducts
+      >
         Add to Cart
       </button>
       </li>
-))}
+      ))}
       </ul>
     </div>
   );
